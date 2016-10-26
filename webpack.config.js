a3c9e4d8bcba2ed08ee0e3b3a607c41c3fa208c7
Nathan Hartman <nathanial.hartman@gmail.com>
2016-10-25 20:31:45 -0700

    basic webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,48 @@
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
-    entry: "./src/index.tsx",
-    output: {
-        filename: "./dist/bundle.js",
-    },
+	entry: ["./src/index.tsx"],
+	output: {
+		filename: "./dist/bundle.js",
+	},
 
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
+	// Enable sourcemaps for debugging webpack's output.
+	devtool: "source-map",
 
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
-    },
+	resolve: {
+		// Add '.ts' and '.tsx' as resolvable extensions.
+		extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+	},
 
-    module: {
-        loaders: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
-            { test: /\.tsx?$/, loader: "ts-loader" }
-        ],
+	module: {
+		loaders: [
+			// All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
+			{ test: /\.tsx?$/, loader: "ts-loader" }
+		],
 
-        preLoaders: [
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { test: /\.js$/, loader: "source-map-loader" }
-        ]
-    },
+		preLoaders: [
+			// All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+			{ test: /\.js$/, loader: "source-map-loader" }
+		]
+	},
 
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
-    },
+	// When importing a module whose path matches one of the following, just
+	// assume a corresponding global variable exists and use that instead.
+	// This is important because it allows us to avoid bundling all of our
+	// dependencies, which allows browsers to cache those libraries between builds.
+	externals: {
+		"react": "React",
+		"react-dom": "ReactDOM"
+	},
+
+	plugins: [
+		new CopyWebpackPlugin([
+			// {output}/file.txt
+			{ from: 'src/index.html' }
+		])
+	],
+
+	devServer: {
+		contentBase: "./dist"
+	}
 };
